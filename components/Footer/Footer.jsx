f60bc5a9d61b32317bd7bc94973a7cdd24f1bf2c
Sujit Roy <sujitroy78148@gmail.com>
2023-05-01 21:47:40 +0530

    initial setup 🚀

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>Footer Component</footer>
+  )
+}
