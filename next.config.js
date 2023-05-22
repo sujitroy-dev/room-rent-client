eed0203385a23d747f3d64f25397e0fd92a69f52
Sujit Roy <sujitroy78148@gmail.com>
2023-05-22 12:15:13 +0530

    env enviromen setup

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
+const webpack = require('webpack')
+const { parsed: myEnv } = require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['placehold.co', 'res.cloudinary.com'],
   },
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
+    return config
+}
 };
 
 module.exports = nextConfig;
