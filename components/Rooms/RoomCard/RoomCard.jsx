4028db6b56afbab3b4b7a12b798ccdc6bc310843
Sujit Roy <sujitroy78148@gmail.com>
2023-05-22 15:33:17 +0530

    wishlist button added on course card

--- components/Rooms/RoomCard/RoomCard.jsx
+++ components/Rooms/RoomCard/RoomCard.jsx
@@ -1,18 +1,49 @@
-import Image from 'next/image';
-import styles from './RoomCard.module.scss';
-import Link from 'next/link';
-export default function RoomCard({title, rent, currency, deposit, security, postedTime, images, location}) {
+import Image from "next/image";
+import styles from "./RoomCard.module.scss";
+import Link from "next/link";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { useState } from "react";
+
+export default function RoomCard({
+  title,
+  rent,
+  currency,
+  deposit,
+  security,
+  postedTime,
+  images,
+  location,
+}) {
   return (
-    <div className={styles['room-card']}>
-      <Image src={images} width={300} height={175} alt={title}/>
+    <div className={styles["room-card"]}>
+      <Image src={images} width={300} height={175} alt={title} />
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.info}>
-        <div className={styles.price}><b>Rent:</b> ₹{rent}</div>
-        <div className={styles.deposit}><b>Deposite:</b> ₹{deposit}</div>
-        <div className={styles.security}><b>Security:</b> ₹{security}</div>
+        <div className={styles.price}>
+          <b>Rent:</b> ₹{rent}
+        </div>
+        <div className={styles.deposit}>
+          <b>Deposite:</b> ₹{deposit}
+        </div>
+        <div className={styles.security}>
+          <b>Security:</b> ₹{security}
+        </div>
       </div>
       <div className={styles.location}>{location}</div>
-      <Link href="/" className={styles['view-btn']}>View All</Link>
+      <div className={styles.action_buttons}>
+        <WishListButton liked={true}/>
+        <Link href="/" className={styles["view-btn"]}>View Details</Link>
+      </div>
     </div>
-  )
+  );
+}
+
+function WishListButton({ liked = false }) {
+  const [isLiked, setLiked] = useState(liked)
+  return (
+    <div className={styles['wishlist-btn']} onClick={()=>setLiked(prev=>!prev)}>
+      {isLiked?<AiFillHeart size="25px" className={styles["wishlist-icon"]} />:
+      <AiOutlineHeart size="25px" className={styles["wishlist-icon"]} />}
+    </div>
+  );
 }
