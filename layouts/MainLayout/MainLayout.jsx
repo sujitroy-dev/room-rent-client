3b11a1a1a20a8b856476fa750604d44f436ad0a3
Sujit Roy <sujitroy78148@gmail.com>
2023-05-23 23:55:34 +0530

    slick-carousel added

--- layouts/MainLayout/MainLayout.jsx
+++ layouts/MainLayout/MainLayout.jsx
@@ -1,24 +1,12 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Footer from "@/components/Footer/Footer";
 import AuthForm from "@/components/Form/Auth/Auth";
 import Header from "@/components/Header/Header";
-import Head from "next/head";
 
 export default function MainLayout({ children }) {
   return (
     <>
-      <Head>
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charSet="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
-      </Head>
       <div
         style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
       >
--- layouts/WithoutSearchLayout/WithoutSearchLayout.jsx
+++ layouts/WithoutSearchLayout/WithoutSearchLayout.jsx
@@ -1,26 +1,14 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Footer from "@/components/Footer/Footer";
 import AuthForm from "@/components/Form/Auth/Auth";
 import HeaderWithoutSearch from "@/components/HeaderWithoutSearch/HeaderWithoutSearch";
-import Head from "next/head";
 import { useState } from "react";
 
 export default function WithoutSearchLayout({ children }) {
   const [isOpen, setOpen] = useState(false);
   return (
     <>
-      <Head>
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charSet="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
-      </Head>
       <div
         style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
       >
