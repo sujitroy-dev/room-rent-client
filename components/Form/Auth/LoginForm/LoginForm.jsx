063ed672ffa3f08a106bbd609e97703c4f6247a0
Sujit Roy <sujitroy78148@gmail.com>
2023-05-23 18:12:27 +0530

    api intigration on loginform

--- components/Form/Auth/LoginForm/LoginForm.jsx
+++ components/Form/Auth/LoginForm/LoginForm.jsx
@@ -1,20 +1,40 @@
+import { useState } from 'react';
 import styles from './LoginForm.module.scss';
 
 export default function LoginForm({changeFormTypeFunc}) {
-  function submitRegistrationForm() {
-    return 0;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const submitForm = async (url, formData) => {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {"Content-Type": "application/json",},
+        body: JSON.stringify(formData)})
+
+      const data = await response.json();
+      if (data.success) changeFormTypeFunc(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  function submitLoginForm() {
+    event.preventDefault();
+    const url = `${process.env.API_BASE}/user/login`;
+    submitForm(url, {email, password});
   }
   return (
     <div className={styles.form__container}>
         <h2>Login</h2>
-        <form onSubmit={submitRegistrationForm}>
+        <form onSubmit={submitLoginForm}>
             <label className={`${styles['full-w-label']}`}>
                 Eamil :
-                <input type="email" name="" id="" required/>
+                <input type="email" name="email" id="email" required value={email} onChange={(e)=>setEmail(e.target.value)}/>
             </label>
             <label className={`${styles['full-w-label']}`}>
                 Password :
-                <input type="email" name="" id="" required/>
+                <input type="password" name="password" id="password" required value={password} onChange={(e)=>setPassword(e.target.value)}/>
             </label>
             <button type="submit" className={`${styles['full-w-label']}`}>Submit</button>
         </form>
