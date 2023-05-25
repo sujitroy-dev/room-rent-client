b1a154bc438c68855a48b313cc95f0f6e2a57561
Sujit Roy <sujitroy78148@gmail.com>
2023-05-26 00:37:53 +0530

    authForm added on mainLayout

--- layouts/MainLayout/MainLayout.jsx
+++ layouts/MainLayout/MainLayout.jsx
@@ -3,18 +3,20 @@ import "slick-carousel/slick/slick-theme.css";
 import Footer from "@/components/Footer/Footer";
 import AuthForm from "@/components/Form/Auth/Auth";
 import Header from "@/components/Header/Header";
+import { useState } from "react";
 
 export default function MainLayout({ children }) {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <>
       <div
-        style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
-      >
+        style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
         <Header />
-        <main style={{ flex: 1 }}>{children}</main>
+          <main style={{ flex: 1 }}>{children}</main>
         <Footer />
       </div>
-      <AuthForm/>
+      <AuthForm open={isOpen} setOpenFun={setOpen}/>
     </>
   );
 }
