bbe67958ab8947bcf9bdb0a069cb09ab1cb403ed
Sujit Roy <sujitroy78148@gmail.com>
2023-05-29 10:35:28 +0530

    header for loggedIn users

--- components/HeaderWithoutSearch/HeaderWithoutSearch.jsx
+++ components/HeaderWithoutSearch/HeaderWithoutSearch.jsx
@@ -1,20 +1,30 @@
-'use client';
+"use client";
 import styles from "./HeaderWithoutSearch.module.scss";
 import Link from "next/link";
 import { AiFillHeart } from "react-icons/ai";
 import { RiUser6Fill } from "react-icons/ri";
-import { useDispatch } from 'react-redux'
-import { makeAuthFormVisible } from '@/redux/features/layout/layoutSlice.js'
+import { useDispatch } from "react-redux";
+import { makeAuthFormVisible } from "@/redux/features/layout/layoutSlice.js";
 import { useEffect, useState } from "react";
+import useParseJWT from "@/hooks/useParseJWT";
 
 export default function HeaderWithoutSearch() {
-  const dispatch = useDispatch()
-  const showAuthFormFunc = ()=>dispatch(makeAuthFormVisible());
-  let isLoggedIn = false;
+  const dispatch = useDispatch();
+  const showAuthFormFunc = () => dispatch(makeAuthFormVisible());
+  const [isLoggedIn, setLoggedIn] = useState(false);
 
-  if (typeof window !== "undefined") {
-    // isLoggedIn = localStorage.getItem("token");
-  }
+  useEffect(() => {
+    let isLoggedIn = localStorage.getItem("token");
+
+    if (isLoggedIn) {
+      const data = useParseJWT(isLoggedIn);
+      if (data.email) return setLoggedIn(true);
+      return setLoggedIn(false)
+    }else{
+      return setLoggedIn(false);
+    }
+  }, []);
+
   return (
     <div>
       <header className={styles.header}>
@@ -27,7 +37,10 @@ export default function HeaderWithoutSearch() {
               <AiFillHeart size="25px" className={styles["wishlist-icon"]} />
             </Link>
             {!isLoggedIn ? (
-              <div className={styles["registier-login"]} onClick={showAuthFormFunc}>
+              <div
+                className={styles["registier-login"]}
+                onClick={showAuthFormFunc}
+              >
                 Login
               </div>
             ) : (
