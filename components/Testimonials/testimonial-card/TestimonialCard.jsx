eb3f06af6a8215d289b7db6a8365a9f34fc58f04
Sujit Roy <sujitroy78148@gmail.com>
2023-06-01 12:53:48 +0530

    user name added as alt attribute on testimonials-card

--- components/Testimonials/testimonial-card/TestimonialCard.jsx
+++ components/Testimonials/testimonial-card/TestimonialCard.jsx
@@ -4,7 +4,7 @@ import styles from './TestimonialCard.module.scss';
 export default function TestimonialCard({ name, designation, image, message}) {
   return (
     <div className={styles['testimonial-card']}>
-      <Image src={image} width={65} height={65} className={styles['profile-img']}/>
+      <Image src={image} width={65} height={65} className={styles['profile-img']} alt={name}/>
       <p className={styles.name}>{name}</p>
       <p className={styles.message}>{message}</p>
       <p className={styles.designation}>{designation}</p>
