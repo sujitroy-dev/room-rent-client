6d0c91cc0d073039e8fc2065bea87cdc51a849b6
Sujit Roy <sujitroy78148@gmail.com>
2023-06-02 00:16:45 +0530

    initial room details page

--- pages/room/[id]/index.jsx
+++ pages/room/[id]/index.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import MainLayout from "@/layouts/MainLayout/MainLayout";
-import WithoutSearchLayout from "@/layouts/WithoutSearchLayout/WithoutSearchLayout";
 
-export default function RoomPage() {
+export default function RoomPage({id}) {
+  const [room , setRoom] = useState()
+
+  async function fetchRooms() {
+    const recentRoomsResponse = await fetch(
+      `${process.env.API_BASE}/room/single/${id}`
+    );
+    const recentRooms = await recentRoomsResponse.json();
+    setRoom(recentRooms.data);
+  }
+  useEffect(() => {
+    fetchRooms();
+  }, [id])
+  
+
   return (
     <MainLayout>
-      <div>Room Page</div>
+      <h1 style={{textAlign: "center", margin: "20px auto"}}>{room?.title}</h1>
+      <p style={{textAlign: "center"}}>{room?.description}</p>
     </MainLayout>
   );
 }
+
+RoomPage.getInitialProps = async ({ query }) => {
+  return { id: query.id };
+};
