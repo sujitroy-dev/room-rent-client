f0e84da2c666ad1f632abe2c17fa6e9f6a04c592
Sujit Roy <sujitroy78148@gmail.com>
2023-06-04 19:27:15 +0530

    testimonil section responsiveness

--- components/Testimonials/Testimonials.jsx
+++ components/Testimonials/Testimonials.jsx
@@ -12,6 +12,22 @@ const settings = {
   speed: 500,
   slidesToShow: 3,
   slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ]
 };
 
 export default function Testimonials() {
