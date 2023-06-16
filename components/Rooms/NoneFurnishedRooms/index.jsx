8d42c427ccdb33ae393633af20fd66b7dc0e3b61
Sujit Roy <sujitroy78148@gmail.com>
2023-06-16 11:57:38 +0530

    none furnished rooms bug fixed

--- components/Rooms/NoneFurnishedRooms/index.jsx
+++ components/Rooms/NoneFurnishedRooms/index.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Rooms from "../Rooms";
 import { useQuery } from "react-query";
+import token from "@/data/token";
 
 export default function NoneFurnishedRooms() {
   async function fetchRooms() {
-    const response = await fetch(`${process.env.API_BASE}/room/none-furnished`);
+    const response = await fetch(`${process.env.API_BASE}/room/none-furnished`,{
+      headers: {
+        credentials: 'include',
+        Authorization: token
+      },
+    });
     return response.json();
   }
 
