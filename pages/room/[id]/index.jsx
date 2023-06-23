f37b6b86d70ef19cc2d7de05b6d60b1c4ddc5b77
Sujit Roy <sujitroy78148@gmail.com>
2023-06-24 01:05:18 +0530

    new breadcrum and api intigration on details pag

--- pages/room/[id]/index.jsx
+++ pages/room/[id]/index.jsx
@@ -2,14 +2,29 @@ import styles from './index.module.css';
 import { useQuery } from "react-query";
 import WithoutSearchLayout from "@/layouts/WithoutSearchLayout/WithoutSearchLayout";
 import { getSingleRoom } from '@/services/apiClients/rooms';
+import Breadcrumb from '@/components/Breadcrumb/Breadcrumb';
+import Image from 'next/image';
 
-export default function RoomPage({id}) {
-  const { data: room } = useQuery(`room-${id}`, ()=>getSingleRoom(id));
+export default function RoomPage({ id }) {
+  const { data: room } = useQuery(`room-${id}`, () => getSingleRoom(id));
 
+  console.log(room);
+  
   return (
     <WithoutSearchLayout>
-      <h1 style={{textAlign: "center", margin: "20px auto"}}>{room?.data?.title}</h1>
-      <p style={{textAlign: "center"}}>{room?.data?.description}</p>
+      <div className="container m-auto">
+        <Breadcrumb />
+        <div className="flex">
+          <div className="">
+            <Image src={room?.data?.pictures?.[0]} width={600} height={600}/>
+          </div>
+          <div className="flex-1">
+            <h1 className="">{room?.data?.title}</h1>
+            <p className="">{room?.data?.description}</p>
+          </div>
+        </div>
+      </div>
+
     </WithoutSearchLayout>
   );
 }
