df6960cbd083d233e2d8e35a57c5f7e21c0df4f1
Sujit Roy <sujitroy78148@gmail.com>
2023-06-24 00:35:22 +0530

    room card with tailwind

--- components/Rooms/RoomCard/RoomCard.jsx
+++ components/Rooms/RoomCard/RoomCard.jsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import styles from "./RoomCard.module.scss";
 import Link from "next/link";
 import { useDispatch } from 'react-redux'
 import { makeAuthFormVisible } from '@/redux/features/layout/layoutSlice.js'
@@ -24,39 +23,33 @@ export default function RoomCard({
   postedTime,
   images,
   location,
-  path='/',
-  liked=false
+  path = '/',
+  liked = false
 }) {
   return (
-    <div className={styles["room-card"]}>
-      <Link href={path}>
-        <Image src={images} width={300} height={175} alt={title} />
-      </Link>
-      <Link href={path}>
-        <h2 className={styles.title}>{title}</h2>
-      </Link>
-      <div className={styles.info}>
-        <div className={styles.type}>
-          <b>Type:</b> {apartment_type}
+    <div className="p-5 bg-white shadow-md rounded-xl w-[300px] mb-3">
+      <Link href={path}><Image src={images} width={300} height={175} alt={title} className="rounded-md mb-2.5 w-full h-[173px]" /></Link>
+      <h6 className="text-md font-medium mb-3"><Link href={path}>{title}</Link></h6>
+      <div className="grid grid-cols-2 gap-y-3 gap-x-2.5 mb-5">
+        <div className="text-sm font-normal text-light-black flex items-center gap-2">
+          <b className="font-medium">Type: </b>{apartment_type}
         </div>
-        <div className={styles.ratings}>
-          <b>Ratings: </b>
-          <AiFillStar size="16px" className={styles["wishlist-icon"]} />
-          <span>4.7</span>
+        <div className="text-sm font-normal text-light-black flex items-center gap-2">
+          <b className="font-medium">Ratings: </b>
+          <div className="text-yellow flex items-center">
+            <AiFillStar size="16px" /><span>4.7</span>
+          </div>
         </div>
-        <div className={styles.price}>
-          <b>Rent:</b> ₹ {rent}
+        <div className="text-sm font-normal text-light-black flex items-center gap-2">
+          <b className="font-medium">Rent: </b>₹ {rent}
         </div>
-        <div className={styles.security}>
-          <b>Security:</b> ₹ {security}
+        <div className="text-sm font-normal text-light-black flex items-center gap-2">
+          <b className="font-medium">Security: </b>₹ {security}
         </div>
       </div>
-      <div className={styles.location}>{location}</div>
-      <div className={styles.action_buttons}>
-        <WishListButton liked={liked} id={id}/>
-        <Link href={path} className={styles["view-btn"]}>
-          View Details
-        </Link>
+      <div className="flex gap-3">
+        <div className="w-10 border border-light-gray rounded-md flex items-center justify-center"><WishListButton liked={liked} id={id} /></div>
+        <Link href={path} className="border border-blue rounded-md px-3 py-2 text-center font-medium text-blue flex-1">view details</Link>
       </div>
     </div>
   );
@@ -70,7 +63,7 @@ function WishListButton({ liked, id }) {
 
   async function likeFunction() {
     const data = await likeRoom(id);
-    
+
     if (data.success) {
       setLiked(true);
       toast.success(data.message);
@@ -86,22 +79,19 @@ function WishListButton({ liked, id }) {
     }
     return data;
   }
-  
+
   function handleLikeDislike() {
-    if(!token) return showAuthFormFunc();
-    if(isLiked) return dislikeFunction();
+    if (!token) return showAuthFormFunc();
+    if (isLiked) return dislikeFunction();
     return likeFunction();
   }
-  
+
   return (
-    <div
-      className={styles["wishlist-btn"]}
-      onClick={handleLikeDislike}
-    >
+    <div onClick={handleLikeDislike} >
       {isLiked ? (
-        <AiFillHeart size="25px" className={styles["wishlist-icon"]} />
+        <AiFillHeart size="25px" className="text-red" />
       ) : (
-        <AiOutlineHeart size="25px" className={styles["wishlist-icon"]} />
+        <AiOutlineHeart size="25px" className="text-red" />
       )}
     </div>
   );
