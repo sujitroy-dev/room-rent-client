40ce4e7eec27238ec92ff04707afd61e08c5efe5
Sujit Roy <sujitroy78148@gmail.com>
2023-06-24 01:05:18 +0530

    new breadcrum and api intigration on details pag

---
+++ components/Breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Breadcrumb() {
+  return (
+    <nav className="flex py-10" aria-label="Breadcrumb">
+      <ol className="inline-flex items-center space-x-1 md:space-x-3">
+        <li className="inline-flex items-center">
+          <Link href="/" className="inline-flex items-center text-md font-medium hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+            Home
+          </Link>
+        </li>
+        <li>
+          <div className="flex items-center">
+            <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+            <Link href="/search" className="ml-1 text-md font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Search</Link>
+          </div>
+        </li>
+        <li aria-current="page">
+          <div className="flex items-center">
+            <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+            <span className="ml-1 text-md font-medium md:ml-2 dark:text-gray-400">Room</span>
+          </div>
+        </li>
+      </ol>
+    </nav>
+  )
+}
