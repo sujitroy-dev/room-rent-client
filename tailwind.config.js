448706fd0bea6bfa41e78a967a1c9b818af43178
Sujit Roy <sujitroy78148@gmail.com>
2023-06-26 16:49:33 +0530

    tailwind color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const { colors: defaultColors } = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -6,15 +7,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      'red': '#FF0000',
-      'light-gray': '#C9C9C9',
-      'light-black': "#00000094",
-      'blue': '#146C94',
-      'yellow': '#FDCD0E',
-      'white': "#ffffff"
+    extend: {
+      colors: {
+        'red': '#FF0000',
+        'light-gray': '#C9C9C9',
+        'light-black': "#00000094",
+        'blue': '#146C94',
+        'yellow': '#FDCD0E',
+        'white': "#ffffff"
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
