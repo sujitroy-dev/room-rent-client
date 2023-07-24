47f6d7837ab4df2e0c26cfb160cf8fbbab854483
Sujit Roy <sujitroy78148@gmail.com>
2023-07-24 19:16:31 +0530

    reusablity of breadcrum

--- components/Breadcrumb/Breadcrumb.tsx
+++ components/Breadcrumb/Breadcrumb.tsx
@@ -1,61 +1,50 @@
 import Link from "next/link";
-import React from "react";
 
-export default function Breadcrumb() {
+type navigation = {
+  title: string;
+  path?: string;
+};
+type Props = {
+  navigations: navigation[];
+};
+
+export default function Breadcrumb({ navigations = [] }: Props) {
   return (
     <nav className="flex py-10" aria-label="Breadcrumb">
       <ol className="inline-flex items-center space-x-1 md:space-x-3">
-        <li className="inline-flex items-center">
-          <Link
-            href="/"
-            className="inline-flex items-center text-md font-medium hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <div className="flex items-center">
-            <svg
-              aria-hidden="true"
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <Link
-              href="/search"
-              className="ml-1 text-md font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
-            >
-              Search
-            </Link>
-          </div>
-        </li>
-        <li aria-current="page">
-          <div className="flex items-center">
-            <svg
-              aria-hidden="true"
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <span className="ml-1 text-md font-medium md:ml-2 dark:text-gray-400">
-              Room
-            </span>
-          </div>
-        </li>
+        {navigations.map((navigation: navigation, index: number) => (
+          <li key={navigation.title + index}>
+            <div className="flex items-center">
+              {index > 0 && (
+                <svg
+                  aria-hidden="true"
+                  className="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              )}
+              {navigation.path ? (
+                <Link
+                  href={navigation.path}
+                  className="ml-1 text-md font-medium text-gray-700 hover:text-blue-600 md:ml-2"
+                >
+                  {navigation.title}
+                </Link>
+              ) : (
+                <span className="ml-1 text-md font-medium text-gray-700 hover:text-blue-600 md:ml-2">
+                  {navigation.title}
+                </span>
+              )}
+            </div>
+          </li>
+        ))}
       </ol>
     </nav>
   );
