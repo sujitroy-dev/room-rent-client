d2485e5f6dc35d21e94628a525b6c1639eca8be9
Sujit Roy <sujitroy78148@gmail.com>
2023-07-25 19:25:41 +0530

    small change

--- pages/dashboard/property/edit/[id].tsx
+++ pages/dashboard/property/edit/[id].tsx
@@ -88,7 +88,7 @@ export default function PropertyEditPage() {
   return (
     <DashboardLayout>
       <div className="py-4 px-6">
-        <h1 className="text-3xl font-semibold mt-1">View Room</h1>
+        <h1 className="text-3xl font-semibold mt-1">Edit Room</h1>
         <Breadcrumb navigations={breadcrumbNavigation} spacing="small" />
         <form className="">
           <DetailsSection header="Property Details">
--- pages/dashboard/property/view/[id].tsx
+++ pages/dashboard/property/view/[id].tsx
@@ -18,7 +18,7 @@ export default function PropertyViewPage() {
   return (
     <DashboardLayout>
       <div className="py-4 px-6">
-        <h1 className="text-3xl font-semibold mt-1">Edit Room</h1>
+        <h1 className="text-3xl font-semibold mt-1">View Room</h1>
         <Breadcrumb navigations={breadcrumbNavigation} spacing="small" />
         <div>Property View Page</div>
       </div>
