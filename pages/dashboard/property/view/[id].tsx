b3be6f89e9f9d0dfc480928b6aef2ea48de1b7bb
Sujit Roy <sujitroy78148@gmail.com>
2023-07-25 18:42:58 +0530

    owner room view page

---
+++ pages/dashboard/property/view/[id].tsx
@@ -0,0 +1,27 @@
+import Breadcrumb from "@/components/Breadcrumb/Breadcrumb";
+import DashboardLayout from "@/layouts/Dashboard";
+
+export default function PropertyViewPage() {
+  const breadcrumbNavigation = [
+    {
+      title: "Dashboard",
+      path: "/dashboard",
+    },
+    {
+      title: "View",
+      path: "",
+    },
+    {
+      title: "Room",
+    },
+  ];
+  return (
+    <DashboardLayout>
+      <div className="py-4 px-6">
+        <h1 className="text-3xl font-semibold mt-1">Edit Room</h1>
+        <Breadcrumb navigations={breadcrumbNavigation} spacing="small" />
+        <div>Property View Page</div>
+      </div>
+    </DashboardLayout>
+  );
+}
